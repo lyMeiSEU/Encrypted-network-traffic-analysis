767a659400ff570ce2959cd95a4becf4810968a2
lyMeiSEU <213173625@seu.edu.cn>
2018-12-16 20:23:51 +0800

    Initial commit

---
+++ Json decoder.py
@@ -0,0 +1,20 @@
+import os, shutil
+import json
+import jsonpath
+
+file=open("FILE.json",'r')
+
+data=json.load(file)
+#print(data)
+#js=json.dumps(json.load(data))
+fr=open('result.json','w',encoding='utf-8')
+for v in data:
+   # print(v)
+    if v['_source']['layers']['ip']['ip.src']=="47.75.56.162":
+        json.dump(v,fr,ensure_ascii=False,indent=4)
+        print(v)
+        #fr.write("\n")
+    if v['_source']['layers']['ip']['ip.dst']=="47.75.56.162":
+        json.dump(v,fr,ensure_ascii=False,indent=4)
+        print(v)
+    
